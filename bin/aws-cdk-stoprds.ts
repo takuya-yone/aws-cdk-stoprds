99692180d6af0b423cf83351cac2ff0796eba6de
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-06-20 01:10:51 +0900

    up

--- bin/aws-cdk-stoprds.ts
+++ bin/aws-cdk-stoprds.ts
@@ -2,11 +2,11 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AwsCdkStoprdsStack } from '../lib/aws-cdk-stoprds-stack';
-import { AwsSolutionsChecks } from 'cdk-nag'
+import { AwsSolutionsChecks } from 'cdk-nag';
 import { Aspects } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-new AwsCdkStoprdsStack(app, 'AwsCdkStoprdsStack', {
-});
+new AwsCdkStoprdsStack(app, 'AwsCdkStopRdsStack');
 
-Aspects.of(app).add(new AwsSolutionsChecks());
+cdk.Tags.of(app).add('Project', 'StopRds');
+// Aspects.of(app).add(new AwsSolutionsChecks());
--- lib/aws-cdk-stoprds-stack.ts
+++ lib/aws-cdk-stoprds-stack.ts
@@ -1,16 +1,67 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import { ScopedAws } from 'aws-cdk-lib';
 
 export class AwsCdkStoprdsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { accountId, region } = new ScopedAws(this);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkStoprdsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const clusterNames: string[] = [
+      'nextjs-docker-aurora',
+      'portal-request-db-test-v2-cluster',
+    ];
+    const clusterArns: string[] = clusterNames.map(
+      (x) => `arn:aws:rds:${region}:${accountId}:cluster:${x}`
+    );
+
+    const duration = cdk.Duration.minutes(15);
+    const wait = new sfn.Wait(this, 'Wait 15min', {
+      time: sfn.WaitTime.duration(duration),
+    });
+
+    const parallel = new sfn.Parallel(this, 'Stop Clusters');
+
+    for (const [index, clusterName] of clusterNames.entries()) {
+      const stoprds_task = new tasks.CallAwsService(
+        this,
+        `StopRdsTask_${index}`,
+        {
+          service: 'rds',
+          action: 'stopDBCluster',
+          parameters: {
+            DbClusterIdentifier: clusterName,
+          },
+          iamResources: clusterArns,
+          iamAction: 'rds:StopDBCluster',
+        }
+      );
+      stoprds_task.addRetry({ maxAttempts: 5, backoffRate: 5 });
+
+      parallel.branch(stoprds_task);
+    }
+
+    const stopRdsStateMachineLogGroup = new logs.LogGroup(
+      this,
+      'stopRdsStateMachineLogGroup',
+      {
+        retention: logs.RetentionDays.ONE_YEAR,
+      }
+    );
+
+    new sfn.StateMachine(this, 'StopRdsStateMachine', {
+      definition: wait.next(parallel),
+      stateMachineName: 'StopRdsStateMachine',
+      tracingEnabled: true,
+      logs: {
+        destination: stopRdsStateMachineLogGroup,
+        level: sfn.LogLevel.ALL,
+      },
+    });
   }
 }
